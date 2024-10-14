a62ac59cbf36942b79c0b21f7e0e7b4cbdf0c888
Rupankar Ghosh <rupankarghosh2012@gmail.com>
2024-10-14 18:17:20 +0530

    Add files via upload

---
+++ student.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Student</title>
+</head>
+<body>
+    <?php
+
+    $id=$_POST["id"];
+    $password=$_POST["password"];
+
+    echo "Welcom<br>";
+    echo "$id <br> $password";
+
+    ?>
+</body>
+</html>
