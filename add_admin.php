fb8a525c8a51fc31954744f92103d74070b52bf4
Rupankar Ghosh <rupankarghosh2012@gmail.com>
2024-10-27 15:14:28 +0530

    Add files via upload

---
+++ add_admin.php
@@ -0,0 +1,113 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Registration Page</title>
+    <style>
+        body {
+            font-family: Arial, sans-serif;
+            background-color: #F3F3FE;
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            height: 100vh;
+            margin: 0;
+        }
+
+        .container {
+            background-color: white;
+            padding: 30px;
+            padding-right: 50px;
+            border-radius: 10px;
+            box-shadow: 0px 0px 15px rgba(0, 0, 0, 0.1);
+            width: 400px;
+            text-align: left;
+        }
+
+        h1 {
+            text-align: center;
+        }
+
+        input[type="text"],
+        input[type="password"],
+        input[type="date"],
+        select {
+            width: 100%;
+            padding: 10px;
+            margin: 10px 0;
+            border: 1px solid #ccc;
+            border-radius: 5px;
+        }
+
+        input[type="submit"] {
+            width: 100%;
+            padding: 10px;
+            background-color: #007bff;
+            color: white;
+            border: none;
+            border-radius: 5px;
+            cursor: pointer;
+            font-size: 16px;
+        }
+
+        input[type="submit"] :hover {
+            background-color: #0056b3;
+        }
+
+        .success {
+            color: green;
+        }
+
+        .error {
+            color: red;
+        }
+    </style>
+</head>
+<body>
+    <div class="container">
+        <h1>Add Student</h1>
+
+        
+        <?php
+        
+        if (isset($_POST["submit"]))  {
+            // Capture form data
+           
+            $id = $_POST['id'];
+            $password = $_POST['password'];
+         
+
+            // Simple validation check
+            if (empty($id) || empty($password)) {
+                echo "<center class=error> All fields are required!</center>";
+            } else {
+                 $mysql=mysqli_connect("localhost","root","","minor_project");
+                 $sql="insert into admin values('$id','$password')";
+                 $mysql->query($sql);
+
+                // $sql="insert into student values(?,?,?,?,?,?)";
+                // $ps=$mysql->prepare($sql);
+                // $ps->bind_param("issssi",$id,$password,$name,$dob,$course,$year);
+                // $ps->execute();
+
+                echo "<center class=success> Registration successful!</center>";
+            }
+        }
+        ?>
+
+        <form method="POST" action="">
+
+            <label for="id">Admin ID:</label>
+            <input type="text" id="id" name="id" placeholder="Enter your ID">
+
+            <label for="password">Password:</label>
+            <input type="password" id="password" name="password" placeholder="Enter your password">
+
+
+            <input type="submit" name="submit" >
+        </form>
+    </div>
+
+</body>
+</html>
---
+++ admin_login.php
@@ -0,0 +1,182 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Sign In Page</title>
+    <style>
+        body {
+            font-family: Arial, sans-serif;
+            background-color: rgb(247, 235, 255);
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            height: 100vh;
+            margin: 0;
+        }
+
+        .container {
+            background-color: none;
+            padding: 20px;
+            border-radius: 10px;
+            /* box-shadow: 0px 0px 15px rgba(0, 0, 0, 0.1); */
+            width: 300px;
+            text-align: center;
+        }
+
+        h2 {}
+
+        input[type="text"],
+        input[type="password"] {
+            width: 80%;
+            padding: 15px;
+            border: none;
+            border-radius: 50px;
+            background-color: transparent;
+        }
+
+        input[type="submit"] {
+            width: 100%;
+            padding: 10px;
+            margin-top: 10px;
+            background-color: #c05cd4;
+            color: white;
+            border: none;
+            border-radius: 50px;
+            cursor: pointer;
+            font-size: 16px;
+        }
+
+        .input {
+            width: 100%;
+            height: 45px;
+            border: none;
+            border-radius: 50px;
+            background-color: #f8e9ff;
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            margin-bottom: 20px;
+
+        }
+
+        input[type="text"]:hover {
+            border: none;
+
+        }
+
+        input[type="submit"]:hover {
+            background-color: #b700ff;
+        }
+
+        input:focus {
+            outline: none;
+
+        }
+
+        .error {
+            color: red;
+            display: none;
+        }
+
+        .img {
+            background-image: url(img/login.svg);
+            background-size: contain;
+            background-color: white;
+            padding: 20px;
+            border-radius: 10px;
+            box-shadow: 0px 0px 15px rgba(0, 0, 0, 0.1);
+            width: 300px;
+            height: 300px;
+        }
+
+        img {
+            height: 400px;
+        }
+
+        .main {
+            background-color: white;
+            padding: 20px;
+            padding-left: 0px;
+            border-radius: 30px;
+            box-shadow: 0px 0px 15px rgba(0, 0, 0, 0.1);
+            width: 800px;
+            height: 400px;
+            display: flex;
+            justify-content: center;
+            align-items: center;
+        }
+
+        .icons {
+
+            right: 100px;
+            height: 20px;
+            width: 10px;
+            margin-right: 20px;
+        }
+    </style>
+</head>
+
+<body>
+
+    <center>
+        <?php
+        if (isset($_POST["submit"])) {
+            $id = $_POST['id'];
+            $password = $_POST['password'];
+
+            $mysql = mysqli_connect("localhost", "root", "", "minor_project");
+
+            if ($mysql === false) {
+                die("ERROR: Could not connect. " . mysqli_connect_error());
+            }
+
+            $sql = "SELECT * FROM admin WHERE id='$id' AND password='$password'";
+            $record_obj = mysqli_query($mysql, $sql);
+
+            $n = mysqli_num_rows($record_obj);
+
+            if ($n > 0) {
+
+                session_start();
+                $_SESSION["id"] = $id;
+                $_SESSION["password"] = $password;
+
+                header("Location: admin.php");
+            } else {
+
+                echo '<script>alert("Invalid login. Please try again.")</script>';
+            }
+
+            mysqli_close($mysql);
+        }
+        ?>
+    </center>
+
+    <div class="main">
+        <img src="img/login.svg">
+        <div class="container">
+            <h2>Log In</h2>
+
+            <!-- Login Form -->
+            <form id="loginForm" method="post" action="">
+                <div class="input">
+                    <input type="text" id="id" name="id" placeholder="Username" required>
+                    <img class="icons" src="img/user.svg">
+                </div>
+
+                <div class="input">
+                    <input type="password" name="password" id="password" placeholder="Password" required>
+                    <img class="icons" src="img/password.svg">
+                </div>
+
+                <input type="submit" name="submit" value="Sign In">
+            </form>
+        </div>
+    </div>
+
+</body>
+
+
+</html>
